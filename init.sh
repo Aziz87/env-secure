82c7815147a6b5474ebaab89c942e1e796154867
azizmamoyan <mailter@mail.ru>
2024-03-25 14:21:20 +0300

    upgrade

---
+++ init.sh
@@ -0,0 +1,8 @@
+get_current_directory() {
+    current_file="${PWD}/${0}"
+    echo "${current_file%/*}"
+}
+CWD=$(get_current_directory)
+echo $CWD;
+sh "$CWD/env-encode.sh";
+sh "$CWD/env-decode.sh";
